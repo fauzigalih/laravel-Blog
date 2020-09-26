5d7c01d032c3abbc71f7f88b7d355d36b21b68cf
fauzigalih <fauzigalihas@gmail.com>
2020-09-26 23:26:13 +0700

    Initial commit

---
+++ resources/views/backend/user/index.blade.php
@@ -0,0 +1,48 @@
+  @extends('layout.admin')
+  @section('title', 'Dashboard')
+  @section('content')
+  <main>
+    <div class="welcome">
+      <p>Welcome, Good Morning</p>
+      <p>Fauzi Galih Aji Saputro</p>
+    </div>
+    <div class="list-card">
+      <div class="blog item-card">
+        <div class="header">
+          <p class="title">Blog</p>
+          <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi fugiat ratione saepe ex, dolores expedita vero architecto nemo voluptate, quod, ducimus corporis odio consequuntur deleniti a aliquam? Temporibus, nostrum.</p>
+        </div>
+        <div class="body">
+          <p>Post<span>: 20</span></p>
+          <p>Tag<span>: 56</span></p>
+          <p>Views<span>: 2000</span></p>
+        </div>
+        <a href=""><p>MORE</p></a>
+      </div>
+      <div class="project item-card">
+        <div class="header">
+          <p class="title">Project</p>
+          <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi fugiat ratione saepe ex, dolores expedita vero architecto nemo voluptate, quod, ducimus corporis odio consequuntur deleniti a aliquam? Temporibus, nostrum.</p>
+        </div>
+        <div class="body">
+          <p>Post<span>: 20</span></p>
+          <p>Tag<span>: 56</span></p>
+          <p>Views<span>: 2000</span></p>
+        </div>
+        <a href=""><p>MORE</p></a>
+      </div>
+      <div class="template item-card">
+        <div class="header">
+          <p class="title">Template</p>
+          <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi fugiat ratione saepe ex, dolores expedita vero architecto nemo voluptate, quod, ducimus corporis odio consequuntur deleniti a aliquam? Temporibus, nostrum.</p>
+        </div>
+        <div class="body">
+          <p>Post<span>: 20</span></p>
+          <p>Tag<span>: 56</span></p>
+          <p>Views<span>: 2000</span></p>
+        </div>
+        <a href=""><p>MORE</p></a>
+      </div>
+    </div>
+  </main>
+  @endsection
