ae97f76cd56e5f4ace8494d0200666e21f87f3b2
fauzigalih <fauzigalihas@gmail.com>
2020-10-03 22:53:23 +0700

    seed db pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        DB::table('pages')->insert([
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus impedit odit ut amet facilis quas dolore, veritatis sunt fuga ipsa.',
+            'contact' => 'contact',
+            'termsofservice' => 'termsofservice',
+            'privacypolice' => 'privacypolice'
+        ]);
     }
 }
