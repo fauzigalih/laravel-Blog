f25497b9cf5c53bc859f36ef4e3826dc9573d7f5
fauzigalih <fauzigalihas@gmail.com>
2020-10-03 22:52:54 +0700

    static pages controller

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Page;
-use Illuminate\Http\Request;
 
 class AdminPageController extends Controller
 {
@@ -18,69 +17,27 @@ class AdminPageController extends Controller
         return view('backend.pages.index', compact('model'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function about()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('backend.pages.about', compact('model'));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    
+    public function contact()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('backend.pages.contact', compact('model'));
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    
+    public function termsofservice()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('backend.pages.termsofservice', compact('model'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    
+    public function privacypolice()
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $model = Page::findOrFail(1);
+        return view('backend.pages.privacypolice', compact('model'));
     }
 }
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Page;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -18,69 +17,27 @@ class PageController extends Controller
         return view('frontend.pages.index', compact('model'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function about()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('frontend.pages.about', compact('model'));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    
+    public function contact()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('frontend.pages.contact', compact('model'));
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Page  $page
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Page $page)
+    
+    public function termsofservice()
     {
-        //
+        $model = Page::findOrFail(1);
+        return view('frontend.pages.termsofservice', compact('model'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Page  $page
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Page $page)
+    
+    public function privacypolice()
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Page  $page
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Page $page)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Page  $page
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Page $page)
-    {
-        //
+        $model = Page::findOrFail(1);
+        return view('frontend.pages.privacypolice', compact('model'));
     }
 }
