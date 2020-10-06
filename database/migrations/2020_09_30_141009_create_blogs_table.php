9ee066d047962398ac9ce1581513ec97f3949af8
fauzigalih <fauzigalihas@gmail.com>
2020-10-06 21:08:42 +0700

    Update 2020_09_30_141009_create_blogs_table.php

--- database/migrations/2020_09_30_141009_create_blogs_table.php
+++ database/migrations/2020_09_30_141009_create_blogs_table.php
@@ -15,6 +15,10 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('article');
+            $table->string('tag');
+            $table->string('url');
             $table->timestamps();
         });
     }
