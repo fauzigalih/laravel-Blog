5811c9fa58e8dcbfc4c4af3de9c4f6f3a38ec212
fauzigalih <fauzigalihas@gmail.com>
2020-10-07 23:14:16 +0700

    fillable and validate data

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tag'
+    ];
+
+    public static function validateData(Request $request)
+    {
+        $request->validate([
+            'tag' => 'required|unique:tags,tag|string',
+        ]);
+    }
 }
