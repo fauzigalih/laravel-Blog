eae5319b897de34c04a9a84446d6a5c35749b5a3
fauzigalih <fauzigalihas@gmail.com>
2020-10-08 21:37:04 +0700

    fillable and validate data model image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Image extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image_url',
+        'reference'
+    ];
+
+    public static function validateData(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'image_url' => 'required',
+            'reference' => 'string'
+        ]);
+    }
 }
