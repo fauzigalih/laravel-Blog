a1830450568c18aed2d89a77ed2b09eba9794483
fauzigalih <fauzigalihas@gmail.com>
2020-10-08 21:42:04 +0700

    all task on tag

---
+++ resources/views/backend/project/_form.blade.php
@@ -0,0 +1,24 @@
+@php
+    $view = Route::currentRouteName() === 'tag.show';
+    $create = Route::currentRouteName() === 'tag.create';
+    $edit = Route::currentRouteName() === 'tag.edit';
+
+    $action = 'admin/tag';
+    if ($edit) $action = 'admin/tag/' . $model->id ?? 0; 
+@endphp
+
+<form action="{{ url($action) }}" method="POST">
+    @if ($edit)
+        @method('PUT')
+    @endif
+    @csrf
+    <input name="tag" type="tag" class="form-control" placeholder="Tag" required
+    @if ($view || $edit)
+        value="{{ $model->tag }}"
+        @if ($view)
+            readonly
+        @endif
+    @endif>
+    <button class="btn btn-success">Save</button>
+    <a href="{{ url()->previous() }}" class="btn btn-danger">Kembali</a>
+</form>
---
+++ resources/views/backend/project/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+@section('title', 'Create Tag')
+@section('content')
+<main>
+    <h2>Create Tag</h2>
+    @include('backend.tag._form')
+</main>
+@endsection    
