51b13b83e05b36e43891531eb169358e6c922ce6
fauzigalih <fauzigalihas@gmail.com>
2020-10-08 21:35:53 +0700

    index, create, store, show, edit, delete on image controller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -25,7 +25,7 @@ class ImageController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.image.create');
     }
 
     /**
@@ -36,7 +36,12 @@ class ImageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Image::validateData($request);
+
+        $fileName = strtolower(str_replace(' ', '-', $request->name)).date('dmy').'.'.$request->image_url->extension();
+        $request->image_url->move(public_path('img/article'), $fileName);
+        Image::create(array_merge($request->all(), ['image_url' => $fileName]));
+        return redirect('admin/image')->with('success', 'Data Berhasil Ditambahkan!');
     }
 
     /**
@@ -47,7 +52,8 @@ class ImageController extends Controller
      */
     public function show(Image $image)
     {
-        //
+        $model = Image::findOrFail($image->id);
+        return view('backend.image.view', compact('model'));
     }
 
     /**
@@ -58,7 +64,8 @@ class ImageController extends Controller
      */
     public function edit(Image $image)
     {
-        //
+        $model = Image::findOrFail($image->id);
+        return view('backend.image.edit', compact('model'));
     }
 
     /**
@@ -81,6 +88,7 @@ class ImageController extends Controller
      */
     public function destroy(Image $image)
     {
-        //
+        Image::destroy($image->id);
+        return redirect('admin/image')->with('danger', 'Data Berhasil Dihapus!');
     }
 }
