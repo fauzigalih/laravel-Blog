b4e6448795923026de12261ba7324c78bbaff28f
fauzigalih <fauzigalihas@gmail.com>
2020-10-12 21:28:28 +0700

    Update index.blade.php

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -9,154 +9,61 @@
         <div class="list-item blog">
           <p class="title">Blog Terbaru</p>
           <p class="desc">Blog disini merupakan </p>
-          <a href="" class="more">Show More</a>
+          <a href="{{ url('blog') }}" class="more">Show More</a>
           <div class="list-card">
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
+            @foreach ($blog as $data)
+              <a href="{{ url('blog/'.$data->url) }}">
+                <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
+                <div class="content-card">
+                  <p>{{ $data->title }}</p>
+                  <div class="uploader">
+                    <img src="img/garuda.jpg" alt="">
+                    <p class="name">Fauzi Galih Aji Saputro</p>
+                    <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
+                  </div>
                 </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
+              </a>
+            @endforeach
           </div>
         </div>
         <div class="list-item project">
           <p class="title">Project Terbaru</p>
           <p class="desc">Project disini merupakan aplikasi yang sudah siap digunakan oleh pengguna untuk menyelesaikan masalah.</p>
-          <a href="" class="more">Show More</a>
+          <a href="{{ url('project') }}" class="more">Show More</a>
           <div class="list-card">
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
+            @foreach ($project as $data)
+              <a href="{{ url('project/'.$data->url) }}">
+                <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
+                <div class="content-card">
+                  <p>{{ $data->title }}</p>
+                  <div class="uploader">
+                    <img src="img/garuda.jpg" alt="">
+                    <p class="name">Fauzi Galih Aji Saputro</p>
+                    <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
+                  </div>
                 </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
+              </a>
+            @endforeach
           </div>
         </div>
         <div class="list-item template">
           <p class="title">Template Terbaru</p>
           <p class="desc">Template disini merupakan tampilan website yang sudah diatur sedemikian rupa dengan ukuran perangkat yang berbeda-beda atau responsive.</p>
-          <a href="" class="more">Show More</a>
+          <a href="{{ url('template') }}" class="more">Show More</a>
           <div class="list-card">
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
+            @foreach ($template as $data)
+              <a href="{{ url('template/'.$data->url) }}">
+                <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
+                <div class="content-card">
+                  <p>{{ $data->title }}</p>
+                  <div class="uploader">
+                    <img src="img/garuda.jpg" alt="">
+                    <p class="name">Fauzi Galih Aji Saputro</p>
+                    <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
+                  </div>
                 </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
-            <a href="">
-              <img src="img/garuda.jpg" alt="">
-              <div class="content-card">
-                <p>Garuda Pancasila Garuda Pancasila</p>
-                <div class="uploader">
-                  <img src="img/garuda.jpg" alt="">
-                  <p class="name">Fauzi Galih Aji Saputro</p>
-                  <p class="date">11 Nov, 2020</p>
-                </div>
-              </div>
-            </a>
+              </a>
+            @endforeach
           </div>
         </div>
       </div>
