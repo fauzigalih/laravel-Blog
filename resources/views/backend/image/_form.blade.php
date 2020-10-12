9cc2702d2d29764ed3a19dc9aceee63456a54423
fauzigalih <fauzigalihas@gmail.com>
2020-10-12 21:27:34 +0700

    Update _form.blade.php

--- resources/views/backend/image/_form.blade.php
+++ resources/views/backend/image/_form.blade.php
@@ -12,24 +12,37 @@
         @method('PUT')
     @endif
     @csrf
-    <input name="name" type="name" class="form-control" placeholder="Name Image" required
-    @if ($view || $edit)
-        value="{{ $model->name }}"
-        @if ($view)
-            readonly
-        @endif
-    @endif>
-    <input name="image_url" type="file" class="form-control-file" required
-    @if ($view)
-        disabled
-    @endif>
-    <input name="reference" type="reference" class="form-control" placeholder="Reference URL"
-    @if ($view || $edit)
-        value="{{ $model->reference }}"
-        @if ($view)
-            disabled
-        @endif
-    @endif>
+    <div class="form-group">
+        <label for="name">Name Image</label>
+        <input name="name" type="name" class="form-control" id="name" placeholder="Name Image" required
+        @if ($view || $edit)
+            value="{{ $model->name }}"
+            @if ($view)
+                readonly
+            @endif
+        @else
+            value="{{ old('name') }}"
+        @endif>
+    </div>
+    <div class="form-group">
+        <label for="image_url">Image</label>
+        <input name="image_url" type="file" class="form-control-file" id="image_url"
+        @if ($create)
+            required
+        @endif>
+      </div>
+    <div class="form-group">
+        <label for="reference">Reference URL</label>
+        <input name="reference" type="reference" class="form-control" id="reference" placeholder="Reference URL" required
+        @if ($view || $edit)
+            value="{{ $model->reference }}"
+            @if ($view)
+                readonly
+            @endif
+        @else
+            value="{{ old('reference') }}"
+        @endif>
+    </div>
     <button class="btn btn-success">Save</button>
     <a href="{{ url()->previous() }}" class="btn btn-danger">Kembali</a>
 </form>
