9f5c4cf5ebb44acf04ab2e7af002e98abfb872c2
fauzigalih <fauzigalihas@gmail.com>
2020-10-18 23:30:54 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,16 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 if(document.querySelector('main.main') || document.querySelector('main.static')) document.body.style.backgroundColor = 'white';
+
+const image_url = document.querySelectorAll('input.image_url');
+const copy_url = document.querySelectorAll('button.copy');
+for (let i = 0; i < copy_url.length; i++) {
+    copy_url[i].addEventListener('click', function() {
+        image_url[i].select();
+        image_url[i].setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied link: " + image_url[i].value);
+    });
+}
+
+
