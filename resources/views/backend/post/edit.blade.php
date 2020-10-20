2fee11f97931c3c7b3c2d360f82c81ee1b464e15
fauzigalih <fauzigalihas@gmail.com>
2020-10-20 22:26:18 +0700

    Create edit.blade.php

---
+++ resources/views/backend/post/edit.blade.php
@@ -0,0 +1,15 @@
+@php
+    $arrRouteAction = explode('\\', Route::currentRouteAction());
+    $arrRoute = explode('@', $arrRouteAction[3]);
+    $controller = $arrRoute[0];
+    $action = $arrRoute[1];
+    $control = str_replace('controller', '', strtolower($controller));
+@endphp
+@extends('layouts.admin')
+@section('title', 'Edit '.ucwords($control))
+@section('content')
+<main>
+    <h2>Edit {{ ucwords($control) }}</h2>
+    @include('backend.post._form')
+</main>
+@endsection
