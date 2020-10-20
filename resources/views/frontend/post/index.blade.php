4976d2e3724e7d633764b6672211f1ed404ffe61
fauzigalih <fauzigalihas@gmail.com>
2020-10-20 22:26:26 +0700

    Update index.blade.php

--- resources/views/frontend/post/index.blade.php
+++ resources/views/frontend/post/index.blade.php
@@ -2,6 +2,9 @@
     $arrRouteAction = explode('\\', Route::currentRouteAction());
     $arrRoute = explode('@', $arrRouteAction[3]);
     $controller = str_replace('Controller', '', $arrRoute[0]);
+
+    $array = ["Coba", "Laravel", "Sekarang"];
+    $arrayLast = end($array);
 @endphp
 @extends('layouts.public')
 @section('title', $controller)
@@ -9,23 +12,23 @@
 <main class="main article">
     <div class="main-header">
         <div class="container-main">
-        <div class="list-item blog">
-            <div class="list-card">
-            @foreach ($model as $data)
-                <a href="{{ url('blog/'.$data->url) }}">
-                <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
-                <div class="content-card">
-                    <p>{{ $data->title }}</p>
-                    <div class="uploader">
-                    <img src="img/garuda.jpg" alt="">
-                    <p class="name">Fauzi Galih Aji Saputro</p>
-                    <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
+            <div class="list-item blog">
+                <div class="list-card">
+                @foreach ($model as $data)
+                    <a href="{{ url('blog/'.$data->url) }}">
+                    <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
+                    <div class="content-card">
+                        <p>{{ $data->title }}</p>
+                        <div class="uploader">
+                        <img src="img/garuda.jpg" alt="">
+                        <p class="name">Fauzi Galih Aji Saputro</p>
+                        <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
+                        </div>
                     </div>
+                    </a>
+                @endforeach
                 </div>
-                </a>
-            @endforeach
             </div>
-        </div>
         </div>
     </div>
 </main>
