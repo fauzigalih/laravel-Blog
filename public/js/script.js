bc3d1170301eaba097a5090adbd86b22d17e5492
fauzigalih <fauzigalihas@gmail.com>
2020-10-21 21:00:49 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 bgcolor_static();
 copy_url();
-// img_render();
+article_buttons();
 
 function bgcolor_static() {
-    // if(document.querySelector('main.main') || document.querySelector('main.static')) document.body.style.backgroundColor = 'white';
+    if(document.querySelector('main.main') || document.querySelector('main.static')) document.body.style.backgroundColor = 'white';
 }
 
 function active_link() {
@@ -16,7 +16,6 @@ function active_link() {
         });
     }
 }
-
 
 function copy_url() {
     const image_url = document.querySelectorAll('input.image_url');
@@ -31,20 +30,41 @@ function copy_url() {
     }
 }
 
-function img_render() {
-    const img_render = document.querySelector('input.img');
-    img_render.replaceWith(parseHTML("<p>Hello</p>"));
+function article_buttons() {
+    const article = document.getElementById('article');
+    const titleparagraf = document.querySelector('div.btn-group.article .titleparagraf');
+    const subtitleparagraf = document.querySelector('div.btn-group.article .subtitleparagraf');
+    const paragraf = document.querySelector('div.btn-group.article .paragraf');
+    const image = document.querySelector('div.btn-group.article .image');
+    const list = document.querySelector('div.btn-group.article .list');
+    const deleteall = document.querySelector('div.btn-group.article .delete');
 
+    titleparagraf.addEventListener('click', function() {
+        if (article.value == '') article.innerHTML = article.value + '<h1>Title</h1>\n<p class="first">Lorem ipsum</p>';
+        else article.innerHTML = article.value + '\n<h1>Title</h1>\n<p class="first">Lorem ipsum</p>';
+    });
 
+    subtitleparagraf.addEventListener('click', function() {
+        if (article.value == '' || article.value == null) article.innerHTML = article.value + '<h2>Sub Title</h2>\n<p class="first">Lorem ipsum</p>';
+        else article.innerHTML = article.value + '\n<h2>Sub Title</h2>\n<p class="first">Lorem ipsum</p>';
+    });
 
+    paragraf.addEventListener('click', function() {
+        if (article.value == '') article.innerHTML = article.value + '<p>Lorem ipsum</p>';
+        else article.innerHTML = article.value + '\n<p>Lorem ipsum</p>';
+    });
+    
+    image.addEventListener('click', function() {
+        if (article.value == '') article.innerHTML = article.value + '<figure>\n\t<img src="" alt="">\n\t<a href="#">Title Image</a>\n</figure>';
+        else article.innerHTML = article.value + '\n<figure>\n\t<img src="" alt="">\n\t<a href="#">Title Image</a>\n</figure>';
+    });
+
+    list.addEventListener('click', function() {
+        if (article.value == '') article.innerHTML = article.value + '<ul>\n\t<li>List 1</li>\n\t<li>List 2</li>\n\t<li>List 3</li>\n</ul>';
+        else article.innerHTML = article.value + '\n<ul>\n\t<li>List 1</li>\n\t<li>List 2</li>\n\t<li>List 3</li>\n</ul>';
+    });
+    
+    deleteall.addEventListener('click', function() {
+        article.innerHTML = '';
+    });
 }
-
-const imgs = document.querySelectorAll('div.content-body-generate img');
-for (let i = 0; i < imgs.length; i++) {
-    let img = imgs[i].replaceWith(imgs[i].src.split('/')[4]);
-    $(imgs[i]).replaceWith($.parseHTML('<img src="http://127.0.0.1:8000/img/post/\+img"'))
-}
-
-// var html = "<p>Goblok</p>";
-// html = $.parseHTML(html);
-// $('p.tolol').replaceWith(html);
