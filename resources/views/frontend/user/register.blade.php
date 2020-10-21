cb51b7e7e0ab1102c6dcfe37722b4b5b42957247
fauzigalih <fauzigalihas@gmail.com>
2020-10-21 21:00:56 +0700

    Update register.blade.php

--- resources/views/frontend/user/register.blade.php
+++ resources/views/frontend/user/register.blade.php
@@ -1,29 +1,37 @@
 @extends('layouts.public')
 @section('title', 'Register')
 @section('content')
-    <main class="static">
-        <form action="">
+    <main class="auth">
+        <h1>Register</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ url('admin/register') }}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="name"></label>
+                <label for="name">Nama Lengkap</label>
                 <input name="name" type="name" class="form-control" placeholder="Nama Lengkap" value="{{ old('name') }}" autofocus required>
             </div>
             <div class="form-group">
-                <label for="email"></label>
-                <input name="email" type="email" class="form-control" placeholder="Email" value="{{ old('email') }}" autofocus required>
+                <label for="email">Email</label>
+                <input name="email" type="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required>
             </div>
             <div class="form-group">
-                <label for="password"></label>
-                <input name="password" type="password" class="form-control" placeholder="Password" value="{{ old('password') }}" autofocus required>
+                <label for="password">Password</label>
+                <input name="password" type="password" class="form-control" placeholder="Password" required>
             </div>
             <div class="form-group">
-                <label for="confirm"></label>
-                <input name="confirm" type="password" class="form-control" placeholder="Ulangi Password" value="{{ old('confirm') }}" autofocus required>
+                <label for="confirm">Ulangi Password</label>
+                <input name="confirm" type="password" class="form-control" placeholder="Ulangi Password" required>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" value="">
-                <label class="form-check-label" for="remember">Remember Me</label>
-            </div>
+            <p>Sudah punya akun? <a href="{{ url('admin/login') }}">Login!</a></p>
+            <button class="btn btn-primary" type="submit">Daftar</button>
         </form>
     </main>
 @endsection
