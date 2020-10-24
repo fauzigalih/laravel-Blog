4b498839173d96d52b320d8ef2da37d917e3df43
fauzigalih <fauzigalihas@gmail.com>
2020-10-24 23:01:51 +0700

    Update tag.blade.php

--- resources/views/frontend/pages/tag.blade.php
+++ resources/views/frontend/pages/tag.blade.php
@@ -17,8 +17,8 @@
                             <img src="{{ asset('img/post/'.$data->thumbnail) }}" alt="{{ $data->thumbnail }}">
                             <p class="title">{{ $data->title }}</p>
                             <div class="uploader">
-                            <img src="img/garuda.jpg" alt="">
-                            <p class="name">Fauzi Galih Aji Saputro</p>
+                            <img src="{{ asset('img/profile/'. $data->user->photo) }}" alt="Photo error">
+                            <p class="name">{{ $data->user->name }}</p>
                             <p class="date">{{ $data->created_at->format('M d, Y') }}</p>
                             </div>
                         </a>
